c782ee1d2238665f5ebb8052eb60b717dc9830c2
jastonlim <flamingfinix@gmail.com>
2022-07-28 18:12:36 +0800

    Basic

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,7 +1,9 @@
-print('hello')
+import requests
+
+url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
+r = requests.get(url)
+data = r.json()
 
 
-print ("hi")
-print ("help")
+print(data)
 
-print('arsonistic bitch')
