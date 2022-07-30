3bcdc028f478b3a838dda57e00dc2956db1ef51f
jastonlim <flamingfinix@gmail.com>
2022-07-30 18:18:50 +0800

    trying

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-print ("hi")
+import requests
+import re, csv
 
-print ("hi")
-print ("sup")
-print ("hi")
+url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
+r = requests.get(url)
+data = r.json()
 
-print ("hi")
-print ("sup")
+# dict_keys(['Meta Data', 'Time Series FX (Weekly)'])
+# print(data["Meta Data"])
+
+import json
+# print(json.dumps(data["Time Series FX (Weekly)"],indent=4 ))
+
+close_data = data["Time Series FX (Weekly)"]
+print(close_data)
+# for close in close_data:
+#     print(close["4. close"])
