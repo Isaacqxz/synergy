2410935ff7c715b410fa88659ffe28101b1fdd6d
jastonlim <flamingfinix@gmail.com>
2022-07-30 15:05:37 +0800

    trying

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,9 +1,40 @@
 import requests
-import re
+import re, csv
 
 url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
 r = requests.get(url)
 data = r.json()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # weekly_average = sum(data)
 
 # [x for x in data if '1.' in str(x)]
@@ -14,10 +45,10 @@ data = r.json()
 
 # # print(data)
 # # print(weekly_average)
-for weekly in data:
-    empty_list=[]
-    weekly = re.findall(r"\'4. close\': \'1.[0-9][0-9][0-9][0-9][0-9]\'", data)
-    empty_list.append(weekly)
+# for weekly in data:
+#     empty_list=[]
+#     weekly = re.findall(r"\'4. close\': \'1.[0-9][0-9][0-9][0-9][0-9]\'", data)
+#     empty_list.append(weekly)
 
 # string = ''.join(data)
 # for weekly in data:
@@ -27,6 +58,6 @@ for weekly in data:
 
 
 
-print(match)
+# print(match)
 # print(data['Time Series FX (Weekly)'])
 
