1adddd92b0382a07c0352b38b47fcd786638bcf0
jastonlim <flamingfinix@gmail.com>
2022-07-31 17:49:11 +0800

    mean of weekly closing forex price ( success )

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import requests
 import re
 
-
 url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
 r = requests.get(url)
 data = r.json()
 
+#data['Time Series FX (Weekly)']
+#check what this is. look at the format of what comes out. identified the weeks are the keys of the dictionary
 
 
-# dict_keys(['Meta Data', 'Time Series FX (Weekly)'])
-# print(data["Meta Data"])
+weeks = list(data['Time Series FX (Weekly)'].keys())
+#take the weeks only and put into list
 
-import json
-# print(json.dumps(data["Time Series FX (Weekly)"],indent=4 ))
+answers = {}
+for i in weeks:
+    answers[i]=float(data['Time Series FX (Weekly)'][i]['4. close'])
+#we need this to be float, not string.
 
-close_data = data["Time Series FX (Weekly)"]
-print(close_data)
-# for close in close_data:
-#     print(close["4. close"])
+#iterate thru all the weeks present, put into a new dictionary called answers as key, value as closing price for that week.
+
+length = len(answers)
+closing_price = list(answers.values())
+mean_closing_price = sum(closing_price)/length
+print(mean_closing_price)
