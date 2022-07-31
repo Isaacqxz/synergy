70aa1141ec097a33e0dcdf1ccecb133ff553f120
jastonlim <flamingfinix@gmail.com>
2022-07-31 12:52:00 +0800

    try

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-print ("hi")
+import requests
+import re
 
-print ("hi")
-print ("sup")
-print ("hi")
 
-print ("hi")
-print ("sup")
+url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+print(data['Time Series FX (Weekly)'])
