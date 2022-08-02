576fa3c8ffa3af97b33f48c660b35bac9495c452
jastonlim <flamingfinix@gmail.com>
2022-08-02 13:15:50 +0800

    save

--- API.py
+++ API.py
@@ -3,16 +3,19 @@ import requests
 
 def api_function():
     
-    function= "CURRENCY_EXCHANGE_RATE"
-    from_symbol= "USD"
-    to_symbol= "SGD"
-    apikey = "8YE528VKW69J7ZJ9"
+    # function= "CURRENCY_EXCHANGE_RATE"
+    # from_symbol= "USD"
+    # to_symbol= "SGD"
+    # apikey = "T6RUC1GIJE0O6U1T"
 
-    url = f'https://www.alphavantage.co/query?function={function}&from_symbol={from_symbol}&to_symbol={to_symbol}&apikey={apikey}'
+    # url = f'https://www.alphavantage.co/query?function={function}&from_symbol={from_symbol}&to_symbol={to_symbol}&apikey={apikey}'
 
-    data = requests.get(url).json()
+    # data = requests.get(url).json()
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
+    r = requests.get(url)
+    data = r.json()
 
-    return float(data[f'Realtime Currency Exchange Rate']['5. exchange Rate'])
+    return float(data['Realtime Currency Exchange Rate']['5. exchange Rate'])
 
 
 
