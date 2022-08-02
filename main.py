e7ab61bbcbce20186deaf53384d43e03de976498
jastonlim <flamingfinix@gmail.com>
2022-08-02 12:55:14 +0800

    why

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import requests , re ,csv
+import API, overheads, profit_and_loss, cash_on_hand
 
-url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
-r = requests.get(url)
-data = r.json()
+def main():
+    forex = API.api_function()
+    max_overheads = overheads.overhead(forex)
+    profit_n_loss_losses = profit_and_loss.profit_and_loss(forex)
+    cash_on_hand_losses = cash_on_hand.cash_on_hand(forex)
 
-#data['Time Series FX (Weekly)']
-#check what this is. look at the format of what comes out. identified the weeks are the keys of the dictionary
+    with open("summary_report.txt", "w") as a:
+        a.write(f"[REAL TIME CURRENCY CONVERSION] USD1 = SGD{forex}\n")
+        a.write(f"[HIGHEST OVERHEAD] {max_overheads[0].upper()}: SGD{max_overheads[1]}\n")
 
+        if cash_on_hand_losses == []:
+            a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+        else:
+            for i in range(len(cash_on_hand_losses)):
+                a.write(f"[CASSH DEFICIT] DAY: {cash_on_hand_losses[i][0]}, AMOUNT: {cash_on_hand_losses[i][1]}\n")
+            
+        if profit_n_loss_losses == []:
+            a.write(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY \n")
+        else:
+            for i in range(len(profit_n_loss_losses)):
+                a.write(f"[PROFIT DEFICIT] DAY: {profit_n_loss_losses[i][0]}, AMOUNT: {profit_n_loss_losses[i][1]}\n")
 
-weeks = list(data['Time Series FX (Weekly)'].keys())
-#take the weeks only and put into list
+    print("output successful")
 
-answers = {}
-for i in weeks:
-    answers[i]=float(data['Time Series FX (Weekly)'][i]['4. close'])
-#we need this to be float, not string.
+print(main())
 
-#iterate thru all the weeks present, put into a new dictionary called answers as key, value as closing price for that week.
-
-length = len(answers)
-closing_price = list(answers.values())
-mean_closing_price = sum(closing_price)/length
-print(mean_closing_price)
