0fed5c0938d1223da69ce1447388c63832c3aee6
jastonlim <flamingfinix@gmail.com>
2022-08-02 13:18:40 +0800

    save

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def profit_and_loss(forex):
     path = str(Path.cwd())
-    path =+ "\csv.report\profit_and_loss_usd.csv"
+    path += "\csv.report\profit_and_loss_usd.csv"
 
     rows = []
     with open(path, "r") as a:
