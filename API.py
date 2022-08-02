619035ee5c7922a4876c99eec986b736519bc6da
jastonlim <flamingfinix@gmail.com>
2022-08-02 12:54:31 +0800

    why

--- API.py
+++ API.py
@@ -1,17 +1,14 @@
-import requests , re ,csv
+import requests
 
-# url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=8YE528VKW69J7ZJ9'
-# r = requests.get(url)
-# data = r.json()
 
 def api_function():
     function= "CURRENCY_EXCHANGE_RATE"
     from_symbol= "USD"
     to_symbol= "SGD"
-    api_key = "8YE528VKW69J7ZJ9"
+    apikey = "8YE528VKW69J7ZJ9"
 
-    url = f'https://www.alphavantage.co/query?function={function}&from_symbol={from_symbol}&to_symbol={to_symbol}&apikey={api_key}'
+    url = f'https://www.alphavantage.co/query?function={function}&from_symbol={from_symbol}&to_symbol={to_symbol}&apikey={apikey}'
 
     data = requests.get(url).json()
 
-    return float(data["Realtime Currency Exchange Rate"]["5. exchange Rate"])
+    return float(data['Realtime Currency Exchange Rate']['5. exchange Rate'])
