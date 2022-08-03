15e34e003747193dee99fa6ac10117d60b7cbc37
jastonlim <flamingfinix@gmail.com>
2022-08-03 19:47:15 +0800

    should be correct

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -10,6 +10,7 @@ def profit_and_loss(forex):
     with open(path, "r") as a:
         csvread = csv.reader(a)
         next(csvread)
+        next(csvread)
 
         for row in csvread:
             rows.append(row)
@@ -17,16 +18,16 @@ def profit_and_loss(forex):
     losses = []
 
     for i in range(len(rows)):
-        if i == 0:
-            continue #how to change this
-    if rows[i][4] < rows[i-1][4]:
-        formula = []
-        formula.append("{:.2f}".format(float(rows[i][0])))
-        formula.append("{:,2f}".format(forex*(int(rows[i-1][4]-int(rows[i][4])))))
+        # if i == 0:
+        #     continue 
+        if rows[i][4] < rows[i-1][4]:
+            formula = []
+            formula.append("{:.2f}".format(float(rows[i][0])))
+            formula.append("{:,2f}".format(forex*(int(rows[i-1][4]-int(rows[i][4])))))
 
-        losses.append(formula)
+            losses.append(formula)
 
-    return losses
+        return losses
 
 
 
