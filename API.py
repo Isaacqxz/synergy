198c805cadbe0c93419e20733edfe8b54fbb69f9
jastonlim <flamingfinix@gmail.com>
2022-08-04 04:22:10 +0800

    f

--- API.py
+++ API.py
@@ -7,7 +7,7 @@ def api_function():
     Function is to extract real time currency exchange from USD to SGD from alphavantage.co
     """
     
-    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SD&apikey=T6RUC1GIJE0O6U1T'
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
     # get the url to extract data from alphavantage
     r = requests.get(url)
     data = r.json()
