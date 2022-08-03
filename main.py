e5bcdfc468f09a23581a1db55e3e457dbc97a675
jastonlim <flamingfinix@gmail.com>
2022-08-04 02:24:50 +0800

    try

--- main.py
+++ main.py
@@ -10,27 +10,20 @@ def overall():
         a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
         a.write(f"[HIGHEST OVERHEAD] {max_overheads[0].upper()}: SGD{max_overheads[1]}\n")
 
-        if cash_on_hand_losses == AttributeError:
-            print("Error for cash_on_hand function")
-        elif cash_on_hand_losses == NameError:
-            print("Error for cash_on_hand function")
-        elif cash_on_hand_losses == []:
-            a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-        elif cash_on_hand_losses != []:
-            for i in range(len(cash_on_hand_losses)):
-                a.write(f"[CASH DEFICIT] DAY: {cash_on_hand_losses[i][0]}, AMOUNT: SGD{cash_on_hand_losses[i][1]}\n")
 
+        if cash_on_hand_losses == []:
+            a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+        else:
+            for i in range(len(cash_on_hand_losses)):
+                a.write(f"[CASH DEFICIT] DAY: {cash_on_hand_losses[i][0]}, AMOUNT: {cash_on_hand_losses[i][1]}\n")
             
 
             
         if profit_n_loss_losses == []:
             a.write(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY \n")
-        elif profit_n_loss_losses != []:
-            for i in range(len(profit_n_loss_losses)):
-                a.write(f"[PROFIT DEFICIT] DAY: {profit_n_loss_losses[i][0]}, AMOUNT: SGD{profit_n_loss_losses[i][1]}\n")
         else:
-            print("Error for profit_n_loss function")
-                
+            for i in range(len(profit_n_loss_losses)):
+                a.write(f"[PROFIT DEFICIT] DAY: {profit_n_loss_losses[i][0]}, AMOUNT: {profit_n_loss_losses[i][1]}\n")
     print("output successful")
 
 print(overall())
