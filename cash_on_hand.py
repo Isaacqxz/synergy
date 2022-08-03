acc8b71eed643fe3710fa2d3890414c09b6e009d
jastonlim <flamingfinix@gmail.com>
2022-08-04 03:54:21 +0800

    f

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -20,7 +20,7 @@ def cash_on_hand(forex):
     for i in range(len(rows)-1):
         if rows[i+1][1] < rows[i][1]:
             formula = []
-            formula.append("{:.2f}".format(float(rows[i+1][0])))
+            formula.append(round(float(rows[i+1][0]),2))
             formula.append("{:.2f}".format(forex*(int(rows[i][1]) - int(rows[i+1][1]))))
             losses.append(formula)
         
--- profit_and_loss.py
+++ profit_and_loss.py
@@ -16,8 +16,8 @@ def profit_and_loss(forex):
     for i in range(len(rows)-1):
         if rows[i+1][1] < rows[i][1]:
             formula = []
-            formula.append((float(rows[i+1][0])).round(2))
-            formula.append((forex*(int(rows[i][4]) - int(rows[i+1][4]))).round(2))
+            formula.append("{:.2f}".format(float(rows[i+1][0])))
+            formula.append("{:.2f}".format(forex*(int(rows[i][4]) - int(rows[i+1][4]))))
             losses.append(formula)
 
     return losses
