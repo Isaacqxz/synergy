6730cf2ed8008b74570765ea4893587ed0227915
jastonlim <flamingfinix@gmail.com>
2022-08-04 03:41:48 +0800

    f

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -17,7 +17,7 @@ def profit_and_loss(forex):
         if rows[i+1][1] < rows[i][1]:
             formula = []
             formula.append((float(rows[i+1][0])).round(2))
-            formula.append("{:.2f}".format(forex*(int(rows[i][4]) - int(rows[i+1][4]))))
+            formula.append((forex*(int(rows[i][4]) - int(rows[i+1][4]))).round(2))
             losses.append(formula)
 
     return losses
