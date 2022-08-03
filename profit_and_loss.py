741abd0042cb615d1a1211162422945ef227e096
jastonlim <flamingfinix@gmail.com>
2022-08-03 23:51:49 +0800

    try

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -24,7 +24,7 @@ def profit_and_loss(forex):
             formula = []
             formula.append("{:.2f}".format(float(rows[i][0])))
             formula.append("{:,2f}".format(forex*(int(rows[i-1][4]-int(rows[i][4])))))
-
+            print(formula)
             losses.append(formula)
 
     return losses
