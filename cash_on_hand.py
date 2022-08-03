a72217d3f8a00d89d963ba47a0bab83d3d21a5bf
jastonlim <flamingfinix@gmail.com>
2022-08-04 03:55:07 +0800

    f

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -21,7 +21,7 @@ def cash_on_hand(forex):
         if rows[i+1][1] < rows[i][1]:
             formula = []
             formula.append(round(float(rows[i+1][0]),2))
-            formula.append("{:.2f}".format(forex*(int(rows[i][1]) - int(rows[i+1][1]))))
+            formula.append(round(forex*(int(rows[i][1]) - int(rows[i+1][1]))),2)
             losses.append(formula)
         
     return losses
