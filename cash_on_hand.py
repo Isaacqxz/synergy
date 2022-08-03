058706482647b05cf266faa1f24a7f9533ba85de
jastonlim <flamingfinix@gmail.com>
2022-08-04 00:17:09 +0800

    commit

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -16,7 +16,7 @@ def cash_on_hand(forex):
         
     losses = []
 
-    for i in range(len(rows)`1):
+    for i in range(len(rows)-1):
         if rows[i+1][1] < rows[i][1]:
             formula = []
             formula.append("{:.2f}".format(float(rows[i+1][0])))
