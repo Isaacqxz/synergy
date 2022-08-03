e39d1edb2e148ecf1505291632cccbbc243c0c30
jastonlim <flamingfinix@gmail.com>
2022-08-04 03:21:03 +0800

    t

--- main.py
+++ main.py
@@ -5,28 +5,25 @@ def overall():
     max_overheads = overheads.overhead(forex)
     profit_n_loss_losses = profit_and_loss.profit_and_loss(forex)
     cash_on_hand_losses = cash_on_hand.cash_on_hand(forex)
-    if overall():
-        with open("summary_report.txt", "w") as a:
-            a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
-            a.write(f"[HIGHEST OVERHEAD] {max_overheads[0].upper()}: SGD{max_overheads[1]}\n")
+    with open("summary_report.txt", "w") as a:
+        a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
+        a.write(f"[HIGHEST OVERHEAD] {max_overheads[0].upper()}: SGD{max_overheads[1]}\n")
 
 
-            if cash_on_hand_losses == []:
-                a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-            else:
-                for i in range(len(cash_on_hand_losses)):
-                    a.write(f"[CASH DEFICIT] DAY: {cash_on_hand_losses[i][0]}, AMOUNT: {cash_on_hand_losses[i][1]}\n")
+        if cash_on_hand_losses == []:
+            a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+        else:
+            for i in range(len(cash_on_hand_losses)):
+                a.write(f"[CASH DEFICIT] DAY: {cash_on_hand_losses[i][0]}, AMOUNT: {cash_on_hand_losses[i][1]}\n")
                 
 
                 
-            if profit_n_loss_losses == []:
-                a.write(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY \n")
-            else:
-                for i in range(len(profit_n_loss_losses)):
-                    a.write(f"[PROFIT DEFICIT] DAY: {profit_n_loss_losses[i][0]}, AMOUNT: {profit_n_loss_losses[i][1]}\n")
-        print("output successful")
-    else:
-        print("unseccesfull")
+        if profit_n_loss_losses == []:
+            a.write(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY \n")
+        else:
+            for i in range(len(profit_n_loss_losses)):
+                a.write(f"[PROFIT DEFICIT] DAY: {profit_n_loss_losses[i][0]}, AMOUNT: {profit_n_loss_losses[i][1]}\n")
+    print("output successful")
 
 print(overall())
 
