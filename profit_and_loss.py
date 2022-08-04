b6affcc9b400b1d822459a4480d50f9f7250399f
jastonlim <flamingfinix@gmail.com>
2022-08-04 23:50:35 +0800

    s

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -16,45 +16,45 @@ def profit_and_loss(forex):
     profit_n_lost_path = Path.cwd()/"csv_report"/"profit_and_loss_usd.csv"
 
 
-    try:
-        with profit_n_lost_path.open(mode="r", encoding = "UTF-8") as file:
-            profit_n_lost_list = []
-            reader = csv.reader(file)
-            next(reader)
-            for line in reader:
-                profit_n_lost_list.append(line)
+    # try:
+    with profit_n_lost_path.open(mode="r", encoding = "UTF-8") as file:
+        profit_n_lost_list = []
+        reader = csv.reader(file)
+        next(reader)
+        for line in reader:
+            profit_n_lost_list.append(line)
 
 
-            losses = 0
+        losses = []
 
-            if losses == 0:
-                file.write("\n[NET PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-                file.close()
-
-            i = []
-            while len(profit_n_lost_list) > i + 1:
-
-                if float(profit_n_lost_list(i+1)[4]) < float(profit_n_lost_list[i][4]):
-
-                    losses = float(profit_n_lost_list[i][4]) - float(profit_n_lost_list[i + 1][4])
-
-                    with report_path.open(mode= "a") as file:
-                        file.write(f"\n[PROFIT DEFICIT] DAY: {profit_n_lost_list[i + 1 ][0]}, AMOUNT: SGD{round((losses *forex),2)}")
-                        file.close()
-
-                i += 1
-
-
-
+        if losses == []:
+            file.write("\n[NET PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
             file.close()
 
-    except:
-        with report_path.open(mode="a") as file:
-            file.write(f"[Cash on hand File Error] There is an error with Cash on hand file. Please try to input correct file name\n")
-            file.close()
+        i = []
+        while len(profit_n_lost_list) > i + 1:
 
-    else:
-        pass
+            if float(profit_n_lost_list(i+1)[4]) < float(profit_n_lost_list[i][4]):
+
+                losses = float(profit_n_lost_list[i][4]) - float(profit_n_lost_list[i + 1][4])
+
+                with report_path.open(mode= "a") as file:
+                    file.write(f"\n[PROFIT DEFICIT] DAY: {profit_n_lost_list[i + 1 ][0]}, AMOUNT: SGD{round((losses *forex),2)}")
+                    file.close()
+
+            i += 1
+
+
+
+        file.close()
+
+    # except:
+    #     with report_path.open(mode="a") as file:
+    #         file.write(f"[Cash on hand File Error] There is an error with Cash on hand file. Please try to input correct file name\n")
+    #         file.close()
+
+    # else:
+    #     pass
 
 
 
