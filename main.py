e3dd3b6274a9f499b17285da60c15a905d6f0ca1
jastonlim <flamingfinix@gmail.com>
2022-08-04 12:58:39 +0800

    s

--- main.py
+++ main.py
@@ -29,14 +29,12 @@ import API, overheads, profit_and_loss, cash_on_hand
 
 def main():
     forex = API.api_function()
+    with open("summary_report.txt", "w") as a:
+        a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
+        a.close()
     overheads.overhead(forex)
     cash_on_hand.cash_on_hand(forex)
     profit_and_loss.profit_and_loss(forex)
-    with open("summary_report.txt", "w") as a:
-        overheads.overhead(forex)
-        cash_on_hand.cash_on_hand(forex)
-        profit_and_loss.profit_and_loss(forex)
-
     return("output successful")
 
 print(main())
