167c93371d69cf87c064837179578fa0662f5b5a
jastonlim <flamingfinix@gmail.com>
2022-08-04 23:36:49 +0800

    s

--- tryin.py
+++ tryin.py
@@ -1,7 +1,14 @@
-import API, csv
-from pathlib import Path
 
+
+
+
+
+
+from pathlib import Path
+import csv, API
 forex = API.api_function()
+# to extract real time currency exchange from USD to SGD from alphavantage.co
+
 
 def cash_on_hand(forex):
     """"
@@ -12,52 +19,72 @@ def cash_on_hand(forex):
     where Cash-on-Hand is lower than the previous
     day and the value difference.
     """
-    cash_on_hand_path = Path.cwd()/"csv_report"/"cash_on_hand_usd.csv"
-    report_path = Path.cwd()/"summary_report.txt"
 
-    try:
-        with cash_on_hand_path.open(mode="r", encoding = "UTF-8") as file:
-            cash_on_hand_list = []
-            reader = csv.reader(file)
-            next(reader)
-            for line in reader:
-                cash_on_hand_list.append(line)
-
-
-            losses = []
-
-            if losses == []:
-                file.write("\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-                file.close()
-
-            i = []
-            while len(cash_on_hand_list) > i + 1:
-
-                if float(cash_on_hand_list(i+1)[1]) < float(cash_on_hand_list[i][1]):
-
-                    losses = float(cash_on_hand_list[i][1]) - float(cash_on_hand_list[i + 1][1])
-
-                    with report_path.open(mode= "a") as file:
-                        file.write(f"\n[CASH DEFICIT] DAY: {cash_on_hand_list[i + 1 ][0]}, AMOUNT: SGD{round((losses *forex),2)}")
-                        file.close()
-
-                i += 1
+    path = str(Path.cwd())+"\csv_report\cash_on_hand_usd.csv"
+    # to extract cash on hand data from MAB game through CSV file
+    rows = []
+    #empty list for file to append into 
+    with open(path, "r") as a:
+        # opening cash_on_hand_usd.csv file
+        csvread = csv.reader(a)
+        next(csvread)
+        # skip header of the file
+        for row in csvread:rows.append(row)
+        # appending cash_on_hand_usd.csv file as a list
+            
+    losses = []
+    # empty list for 
 
 
+    for i in range(len(rows)-1):
+        # Using for i is a variable.
+        # The range of the list is the number of rows, which we use len to count, -1 since we are skipping the header and do not want to include it as a row
+        if rows[i+1][1] < rows[i][1]:
+            # using if function to check every 
+            formula = []
+            formula.append(round(float(rows[i+1][0]),2))
+            formula.append(round(forex*(int(rows[i][1]) - int(rows[i+1][1])),2))
+            losses.append(formula)
+        
+    return losses
 
-            file.close()
 
-    except:
-        with report_path.open(mode="a") as file:
-            file.write(f"[Cash on hand File Error] There is an error with Cash on hand file. Please try to input correct file name\n")
-            file.close()
+with open("summary_report.txt", "a") as a:
+    if cash_on_hand(forex) == []:
+        a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
 
     else:
-        pass
+        for i in range(len(cash_on_hand(forex))):
+            a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: SGD{cash_on_hand(forex)[i][1]}\n")
 
 
 
 
+            
+            # return losses
+
+    # with open("summary_report.txt", "a") as a:
+    #     if cash_on_hand(forex) == []:
+    #         a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+
+    #     else:
+    #         for i in range(len(cash_on_hand(forex))):
+    #             a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: SGD{cash_on_hand(forex)[i][1]}\n")
+    # except:
+    #     with open("summary_report.txt", "a") as a:
+    #         a.write("help")
+
+    # else:
+    #     pass
+        # with open("summary_report.txt", "a") as a:
+        #     if cash_on_hand(forex) == []:
+        #         a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+
+        #     else:
+        #         for i in range(len(cash_on_hand(forex))):
+        #             a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: SGD{cash_on_hand(forex)[i][1]}\n")
+
+                
 
 
 
