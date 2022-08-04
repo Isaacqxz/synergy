e7a5fe940418610c7177307928317b5d5e05738a
jastonlim <flamingfinix@gmail.com>
2022-08-04 18:44:05 +0800

    s

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -5,23 +5,32 @@ forex = API.api_function()
 
 def cash_on_hand(forex):
     path = str(Path.cwd())+"\csv_report\cash_on_hand_usd.csv"
-    try:
-        rows = []
-        with open(path, "r") as a:
-            csvread = csv.reader(a)
-            next(csvread)
-            for row in csvread:rows.append(row)
+    # try:
+    rows = []
+    with open(path, "r") as a:
+        csvread = csv.reader(a)
+        next(csvread)
+        for row in csvread:rows.append(row)
             
-        losses = []
+    losses = []
 
-        for i in range(len(rows)-1):
-            if rows[i+1][1] < rows[i][1]:
-                formula = []
-                formula.append(round(float(rows[i+1][0]),2))
-                formula.append(round(forex*(int(rows[i][1]) - int(rows[i+1][1])),2))
-                losses.append(formula)
+    for i in range(len(rows)-1):
+        if rows[i+1][1] < rows[i][1]:
+            formula = []
+            formula.append(round(float(rows[i+1][0]),2))
+            formula.append(round(forex*(int(rows[i][1]) - int(rows[i+1][1])),2))
+            losses.append(formula)
+        with open("summary_report.txt", "a") as a:
+            a.write(f"[CASH DEFICIT] DAY: {losses(forex)[i][0]}, AMOUNT: SGD{losses(forex)[i][1]}\n")
+
+    if losses == []:
+        with open("summary_report.txt", "a") as a:
+            a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+
+
+
             
-            return losses
+            # return losses
 
     # with open("summary_report.txt", "a") as a:
     #     if cash_on_hand(forex) == []:
@@ -30,18 +39,19 @@ def cash_on_hand(forex):
     #     else:
     #         for i in range(len(cash_on_hand(forex))):
     #             a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: SGD{cash_on_hand(forex)[i][1]}\n")
-    except:
-        with open("summary_report.txt", "a") as a:
-            a.write("help")
+    # except:
+    #     with open("summary_report.txt", "a") as a:
+    #         a.write("help")
 
-    else:
-        with open("summary_report.txt", "a") as a:
-            if cash_on_hand(forex) == []:
-                a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+    # else:
+    #     pass
+        # with open("summary_report.txt", "a") as a:
+        #     if cash_on_hand(forex) == []:
+        #         a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
 
-            else:
-                for i in range(len(cash_on_hand(forex))):
-                    a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: SGD{cash_on_hand(forex)[i][1]}\n")
+        #     else:
+        #         for i in range(len(cash_on_hand(forex))):
+        #             a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: SGD{cash_on_hand(forex)[i][1]}\n")
 
                 
 
