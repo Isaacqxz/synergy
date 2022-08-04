05ba68c0beb014d31ee93af101aa1f5dc9ac27d5
jastonlim <flamingfinix@gmail.com>
2022-08-04 23:29:39 +0800

    s

--- tryin.py
+++ tryin.py
@@ -49,7 +49,7 @@ def cash_on_hand(forex):
 
     except:
         with report_path.open(mode="a") as file:
-            file.write("\n[COH File Error] File is missing or not renamed properly. Ensure that file is named 'cash_on_hand_usd.csv")
+            file.write(f"[Cash on hand File Error] There is an error with Cash on hand file. Please ensure that file is correct\n")
             file.close()
 
     else:
