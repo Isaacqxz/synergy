be5df50268c8f995bd7f3f0fb28df01a9db413bf
jastonlim <flamingfinix@gmail.com>
2022-08-04 19:20:53 +0800

    s

--- overheads.py
+++ overheads.py
@@ -4,34 +4,25 @@ from pathlib import Path
 forex = API.api_function()
 
 def overhead(forex):
-    
     path = str(Path.cwd())+"\csv_report\overheads.csv"
-    try:
-        rows = []
-        with open(path, "r") as a:
-            csvreader = csv.reader(a)
-            next(csvreader)
 
-            for row in csvreader:rows.append(row)
+    rows = []
+    with open(path, "r") as a:
+        csvreader = csv.reader(a)
+        next(csvreader)
 
-        max = 0
-        # this is so that the first number in overheads.csv can be max since all overheads is > 0. After first number is max, the code will use the max and compare with subsequence data to aquire new max
-        Highest_overhead = []
+        for row in csvreader:rows.append(row)
 
-        for i in range(len(rows)):
-            if max < float(rows[i][1]):
-                max = float(rows[i][1])
-                Highest_overhead = [rows[i][0], round((max * forex),2)]
+    max = 0
+    # this is so that the first number in overheads.csv can be max since all overheads is > 0. After first number is max, the code will use the max and compare with subsequence data to aquire new max
+    Highest_overhead = []
 
-        return Highest_overhead
+    for i in range(len(rows)):
+        if max < float(rows[i][1]):
+            max = float(rows[i][1])
+            Highest_overhead = [rows[i][0], round((max * forex),2)]
 
-        with open("summary_report.txt", "a") as a:
-            a.write(f"[HIGHEST OVERHEAD] {overhead(forex)[0].upper()}: SGD{overhead(forex)[1]}\n")
+    return Highest_overhead
 
-    except:
-        with open("summary_report.txt", "a") as a:
-            a.write("problem")
-    else:
-        with open("summary_report.txt", "a") as a:
-            a.write(f"[HIGHEST OVERHEAD] {overhead(forex)[0].upper()}: SGD{overhead(forex)[1]}\n")
-        # pass
+with open("summary_report.txt", "a") as a:
+    a.write(f"[HIGHEST OVERHEAD] {overhead(forex)[0].upper()}: SGD{overhead(forex)[1]}\n")
