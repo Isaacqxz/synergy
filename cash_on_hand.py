8ed075096cb09c958aa6882d7b4f5f4c5606aa6c
jastonlim <flamingfinix@gmail.com>
2022-08-04 13:55:23 +0800

    s

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -29,7 +29,7 @@ def cash_on_hand(forex):
 with open("summary_report.txt", "a") as a:
     if cash_on_hand(forex) == []:
         a.write(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-        a.close()
+        # a.close()
     else:
         for i in range(len(cash_on_hand(forex))):
             a.write(f"[CASH DEFICIT] DAY: {cash_on_hand(forex)[i][0]}, AMOUNT: {cash_on_hand(forex)[i][1]}\n")
