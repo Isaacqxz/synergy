17a2d86995224f1a461084d5bfc12a9a4cafc60f
jastonlim <flamingfinix@gmail.com>
2022-08-04 21:59:25 +0800

    s

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,20 +1,40 @@
 from pathlib import Path
 import csv, API
 forex = API.api_function()
+# to extract real time currency exchange from USD to SGD from alphavantage.co
 
 
 def cash_on_hand(forex):
+    """"
+    function will compute
+    the difference in Cash-on-Hand between each
+    day. If Cash-on-Hand is not consecutively higher
+    each day, the program will highlight the day
+    where Cash-on-Hand is lower than the previous
+    day and the value difference.
+    """
+
     path = str(Path.cwd())+"\csv_report\cash_on_hand_usd.csv"
+    # to extract cash on hand data from MAB game through CSV file
     rows = []
+    #empty list for file to append into 
     with open(path, "r") as a:
+        # opening cash_on_hand_usd.csv file
         csvread = csv.reader(a)
         next(csvread)
+        # skip header of the file
         for row in csvread:rows.append(row)
+        # appending cash_on_hand_usd.csv file as a list
             
     losses = []
+    # empty list for 
+
 
     for i in range(len(rows)-1):
+        # Using for i is a variable.
+        # The range of the list is the number of rows, which we use len to count, -1 since we are skipping the header and do not want to include it as a row
         if rows[i+1][1] < rows[i][1]:
+            # using if function to check every 
             formula = []
             formula.append(round(float(rows[i+1][0]),2))
             formula.append(round(forex*(int(rows[i][1]) - int(rows[i+1][1])),2))
