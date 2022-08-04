ed5be91631ecbe262b7b95e4409f6902a355ec1d
S10221133 <S10221133@connect.np.edu.sg>
2022-08-04 20:34:25 +0800

    z

--- API.py
+++ API.py
@@ -25,5 +25,25 @@ with open("summary_report.txt", "w") as a:
     a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
 
 
+import requests 
+from pathlib import Path
 
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
+# get the url to extract data from alphavantage
+r = requests.get(url)
+data = r.json()
+
+
+def api_function():
+    """
+    Function is to extract real time currency exchange from USD to SGD from alphavantage.co
+    """
+    currency_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])  
+
+    summary_report_path = Path.cwd()/"summary_report.txt"
+    forex = api_function()
+    with summary_report_path.open(mode="w") as file:
+        file.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
+
+    return currency_rate
 
