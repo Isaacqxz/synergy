1e83d51562c6c61161c30d4291149bfad0a53a15
jastonlim <flamingfinix@gmail.com>
2022-08-04 19:29:32 +0800

    s

--- API.py
+++ API.py
@@ -2,24 +2,27 @@
 from turtle import hideturtle
 import requests
 
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
+# get the url to extract data from alphavantage
+link = requests.get(url)
+data = link.json()
+
 
 def api_function():
     """
     Function is to extract real time currency exchange from USD to SGD from alphavantage.co
     """
     
-    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
-    # get the url to extract data from alphavantage
-    link = requests.get(url)
-    data = link.json()
+    # url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
+    # # get the url to extract data from alphavantage
+    # link = requests.get(url)
+    # data = link.json()
 
-    return float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    forex = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
 
-forex = api_function()
+    with open("summary_report.txt", "w") as a:
+        a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
 
-with open("summary_report.txt", "w") as a:
-    a.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}\n")
-
-
+    return forex
 
 
