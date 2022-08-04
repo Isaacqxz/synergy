0ef45e6b353c1fa4f94947c9ae0180206e79059a
jastonlim <flamingfinix@gmail.com>
2022-08-04 20:45:15 +0800

    s

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -5,21 +5,22 @@ forex = API.api_function()
 
 def cash_on_hand(forex):
     path = str(Path.cwd())+"\csv_report\cash_on_hand_usd.csv"
+
     rows = []
     with open(path, "r") as a:
         csvread = csv.reader(a)
         next(csvread)
         for row in csvread:rows.append(row)
-            
+                
     losses = []
 
-    for i in range(len(rows)-1):
+    while i in range(len(rows)-1):
         if rows[i+1][1] < rows[i][1]:
             formula = []
             formula.append(round(float(rows[i+1][0]),2))
             formula.append(round(forex*(int(rows[i][1]) - int(rows[i+1][1])),2))
             losses.append(formula)
-        
+            
     return losses
 
 
--- try.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=T6RUC1GIJE0O6U1T'
-# get the url to extract data from alphavantage
-r = requests.get(url)
-data = r.json()
-
-print(data.keys())
