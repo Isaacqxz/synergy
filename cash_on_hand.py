a53d9272dbc17e45116ab374d36221a6ec3ec546
jastonlim <flamingfinix@gmail.com>
2022-08-05 12:50:55 +0800

    w

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -13,49 +13,49 @@ def cash_on_hand(forex):
     cash_on_hand_path = Path.cwd()/"csv_report"/"cash_on_hand_usd.csv"
 
 
-    try:
-        with cash_on_hand_path.open(mode="r", encoding = "UTF-8") as file:
-            cash_on_hand_list = []
-            reader = csv.reader(file)
-            next(reader)
-            for line in reader:
-                cash_on_hand_list.append(line)
+    # try:
+    with cash_on_hand_path.open(mode="r", encoding = "UTF-8") as file:
+        cash_on_hand_list = []
+        reader = csv.reader(file)
+        next(reader)
+        for line in reader:
+            cash_on_hand_list.append(line)
 
 
 
 
-            i = 0
-            losses = []
+        i = 0
+        losses = []
 
-            while len(cash_on_hand_list) > i + 1:
+        while len(cash_on_hand_list) > i + 1:
 
-                if float(cash_on_hand_list[i+1][1]) < float(cash_on_hand_list[i][1]):
+            if float(cash_on_hand_list[i+1][1]) < float(cash_on_hand_list[i][1]):
 
-                    losses = float(cash_on_hand_list[i][1]) - float(cash_on_hand_list[i + 1][1])
+                losses = float(cash_on_hand_list[i][1]) - float(cash_on_hand_list[i + 1][1])
 
-                    with report_path.open(mode= "a") as file:
-                        file.write(f"\n[CASH DEFICIT] DAY: {cash_on_hand_list[i + 1 ][0]}, AMOUNT: SGD{round((losses *forex),2)}")
-                        file.close()
+                with report_path.open(mode= "a") as file:
+                    file.write(f"\n[CASH DEFICIT] DAY: {cash_on_hand_list[i + 1 ][0]}, AMOUNT: SGD{round((losses *forex),2)}")
+                    file.close()
 
-                i += 1
-
-
-
-            if losses == []:
-                file.write("\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
-                file.close()
+            i += 1
 
 
 
+        if losses == []:
+            file.write("\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
             file.close()
 
-    except:
-        with report_path.open(mode="a") as file:
-            file.write(f"[Cash on hand File Error] There is an error with Cash on hand file. Please try to input correct file name\n")
-            file.close()
 
-    else:
-        pass
+
+        file.close()
+
+    # except:
+    #     with report_path.open(mode="a") as file:
+    #         file.write(f"[Cash on hand File Error] There is an error with Cash on hand file. Please try to input correct file name\n")
+    #         file.close()
+
+    # else:
+    #     pass
             
 
 
