8dab11e2da671941dddbfc42c4a4fa96c08e82b8
jastonlim <flamingfinix@gmail.com>
2022-08-05 11:43:52 +0800

    k

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import API, overheads, profit_and_loss, cash_on_hand, tryin
 def main():
     forex = API.api_function()    
     overheads.overhead(forex)
-    tryin.coh_function(forex)
+    cash_on_hand.coh_function(forex)
     profit_and_loss.profit_and_loss(forex)
     return("output successful")
 
