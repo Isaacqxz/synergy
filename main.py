ffe777211620c1abd2ddf44facc35362bdb99b30
S10221133 <S10221133@connect.np.edu.sg>
2022-08-07 00:34:41 +0800

    s

--- main.py
+++ main.py
@@ -2,12 +2,3 @@ import API, overheads, profit_and_loss, cash_on_hand, tryin
 # import from API, overheads, profit_and_loss, cash_on_hand, tryin into main.py
 
 #defining function
-def main():
-    forex = API.api_function()    
-    overheads.overhead(forex)
-    cash_on_hand.cash_on_hand(forex)
-    profit_and_loss.profit_and_loss(forex)
-    return("output successful")
-
-#printing the function
-print(main())
